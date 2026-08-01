4de7461259750382e6a0f40afac7e2dab115bbfb
Sikandar Mandal <sikandarmandal.dev@gmail.com>
2026-08-01 18:26:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,7 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
+const app = require("./app");
 
-// dotenv.config();
+const PORT = process.env.PORT || 8080;
 
-// const authRoutes = require("./routes/authRoutes");
-// const transactionRoutes = require("./backend/routes/transactionRoute");
-
-// const app = express();
-// const PORT = process.env.PORT || 5678;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/transactions", transactionRoutes);
-
-// app.get("/", (req, res) => {
-//     res.send("Welcome to TaxPal Backend");
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
